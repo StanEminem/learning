cd72c30bbc369940af19edc61f5a18d514002d63
Yanyan An <yaan@cisco.com>
2017-06-26 17:26:49 +0300

    update project structure

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var ejs = require('ejs');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'public'));
-app.set('view engine', 'jade');
+var viewPath = path.join(__dirname, 'public/');
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.set('views', viewPath);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/home', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'public/home.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,12 +36,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('page not found');
 });
 
 module.exports = app;
